b6735fa04b8ec9d67e51a38a3fe55d1107ce6ac8
The Magician <magic-modules@google.com>
2019-08-16 12:24:24 -0700

    Bug fixes for GCP modules (#60664)

--- lib/ansible/modules/cloud/google/gcp_compute_disk_info.py
+++ lib/ansible/modules/cloud/google/gcp_compute_disk_info.py
@@ -102,7 +102,7 @@ items:
       type: str
     lastDetachTimestamp:
       description:
-      - Last dettach timestamp in RFC3339 text format.
+      - Last detach timestamp in RFC3339 text format.
       returned: success
       type: str
     labels:
